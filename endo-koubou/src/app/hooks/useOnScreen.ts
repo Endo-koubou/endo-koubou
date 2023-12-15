5ea4269e4b5e4a008d90dbb3dc1b6e1e8f6837ec
PK-Yakkun <mokkun515@gmail.com>
2023-12-15 21:27:29 +0900

    fix: refの参照部分をローカル変数に修正

--- endo-koubou/src/app/hooks/useOnScreen.ts
+++ endo-koubou/src/app/hooks/useOnScreen.ts
@@ -16,13 +16,15 @@ export function useOnScreen(): [
       }
     });
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    const currentRef = ref.current;
+
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, [isIntersecting]);
