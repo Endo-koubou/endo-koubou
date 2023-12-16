cb6aee9a627ddf2c3808400f23bd8e07d0fada89
PK-Yakkun <mokkun515@gmail.com>
2023-12-16 12:05:21 +0900

    update: 作品詳細に公演期間追加、資料集のセクション追加

--- endo-koubou/src/app/components/organisms/WorkDetailSection/index.tsx
+++ endo-koubou/src/app/components/organisms/WorkDetailSection/index.tsx
@@ -3,6 +3,7 @@ import { Work } from "@/app/interface/work";
 import { Typography } from "@/app/components/atoms";
 import { TransButton } from "@/app/components/molecules";
 import styles from "./work_detail_section.module.scss";
+import { DateUtils } from "@/app/util/formatDate";
 
 export type WorksSectionProps = {
   detail: Work;
@@ -11,37 +12,93 @@ export type WorksSectionProps = {
 
 export function WorkDetailSection({ detail, className }: WorksSectionProps) {
   return (
-    <div className={`${styles.contents} ${className}`}>
-      <section>
-        <Image
-          src={detail.imageData.url}
-          width={detail.imageData.width}
-          height={detail.imageData.height}
-          alt={detail.title}
-          style={{ width: "100%", height: "auto" }}
-        />
-        <div className={styles.info}>
-          <Typography variant="h3" className={styles.info_title}>
-            作品詳細
+    <>
+      <div className={`${styles.contents} ${className}`}>
+        <section>
+          <Image
+            src={detail.imageData.url}
+            width={detail.imageData.width}
+            height={detail.imageData.height}
+            alt={detail.title}
+            style={{ width: "100%", height: "auto" }}
+          />
+          <div className={styles.info}>
+            <Typography variant="h3" className={styles.info_title}>
+              作品詳細
+            </Typography>
+            <dl className={styles.info_data}>
+              <dt>
+                <Typography variant="h4">劇団名</Typography>
+              </dt>
+              <dd>{detail.companyName}</dd>
+              <dt>
+                <Typography variant="h4">劇場</Typography>
+              </dt>
+              <dd>{detail.theaterName}</dd>
+              <dt>
+                <Typography variant="h4">公演期間</Typography>
+              </dt>
+              <dd>
+                {DateUtils.toYearMonthFormat(detail.performanceStart)}
+                {" ~ "}
+                {DateUtils.toYearMonthFormat(detail.performanceEnd)}
+              </dd>
+              <dt>
+                <Typography variant="h4">演出家</Typography>
+              </dt>
+              <dd>{detail.director}</dd>
+              <dt>
+                <Typography variant="h4">照明デザイナー</Typography>
+              </dt>
+              <dd>{detail.lightingDesigner}</dd>
+            </dl>
+          </div>
+        </section>
+      </div>
+      {detail.archiveImages && detail.archiveImages.length > 1 && (
+        <>
+          <Typography variant="h2" className={styles.document_title}>
+            資料画像
           </Typography>
-          <dl className={styles.info_data}>
-            <dt>劇団名</dt>
-            <dd>{detail.companyName}</dd>
-            <dt>劇場</dt>
-            <dd>{detail.theaterName}</dd>
-            <dt>演出家</dt>
-            <dd>{detail.director}</dd>
-            <dt>照明デザイナー</dt>
-            <dd>{detail.lightingDesigner}</dd>
-          </dl>
-        </div>
-      </section>
-      <section>
-        <Typography variant="h2">資料画像</Typography>
-      </section>
-      <section>
-        <Typography variant="h2">制作風景</Typography>
-      </section>
+          <section className={styles.document_section}>
+            <>
+              <div className={styles.images}>
+                {detail.archiveImages.map((imageData, index) => (
+                  <div key={index} className={styles.image}>
+                    <Image
+                      src={imageData.url}
+                      layout="fill"
+                      alt={`資料画像${index + 1}`}
+                    />
+                  </div>
+                ))}
+              </div>
+            </>
+          </section>
+        </>
+      )}
+      {detail.productionImages && detail.productionImages.length > 1 && (
+        <>
+          <Typography variant="h2" className={styles.document_title}>
+            制作風景
+          </Typography>
+          <section className={styles.document_section}>
+            <>
+              <div className={styles.images}>
+                {detail.productionImages.map((imageData, index) => (
+                  <div key={index} className={styles.image}>
+                    <Image
+                      src={imageData.url}
+                      layout="fill"
+                      alt={`制作風景${index + 1}`}
+                    />
+                  </div>
+                ))}
+              </div>
+            </>
+          </section>
+        </>
+      )}
       <div className={styles.back_button}>
         <TransButton
           label="作品一覧へ戻る"
@@ -49,6 +106,6 @@ export function WorkDetailSection({ detail, className }: WorksSectionProps) {
           path={"/works"}
         />
       </div>
-    </div>
+    </>
   );
 }
