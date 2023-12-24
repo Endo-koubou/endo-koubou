447e11835ffb21ef8b25daf48c92d0f1ec65470c
PK-Yakkun <mokkun515@gmail.com>
2023-12-24 18:39:51 +0900

    update: MV画像ロード完了後に描画開始するよう改修

--- endo-koubou/src/app/components/organisms/MainVisual/index.tsx
+++ endo-koubou/src/app/components/organisms/MainVisual/index.tsx
@@ -21,6 +21,7 @@ const images_sp = [
 ];
 
 export function MainVisual() {
+  const [isLoadedFirstImage, setIsLoadedFirstImage] = useState(false);
   const [currentSlide, setCurrentSlide] = useState(0);
   const isPc = useResponsive("pc");
 
@@ -31,55 +32,63 @@ export function MainVisual() {
     return () => clearTimeout(timer);
   }, [currentSlide]);
 
+  const handleImageLoad = () => {
+    setIsLoadedFirstImage(true);
+  };
+
   return (
     <section className={styles.mv} style={{ height: "100vh" }}>
-      <div className={styles.title}>
-        <FadeIn fadeType="in">
-          <Image
-            src="/images/logo_endo-koubou.png"
-            width={75}
-            height={68}
-            alt="ENDO工房のロゴ画像"
-            className={styles.logo}
-          />
-        </FadeIn>
-        <MvTitle />
-      </div>
-      {isPc
-        ? images_pc.map((image, index) => (
-            <div
-              key={image}
-              className={`${
-                index === currentSlide
-                  ? styles.activeSlide
-                  : styles.inactiveSlide
-              } ${styles.mv_pc}`}
-            >
+      {isLoadedFirstImage && (
+        <>
+          <div className={styles.title}>
+            <FadeIn fadeType="in">
               <Image
-                src={image}
-                alt={`Slide ${index + 1}`}
-                layout="fill"
-                objectFit="cover"
+                src="/images/logo_endo-koubou.png"
+                width={75}
+                height={68}
+                alt="ENDO工房のロゴ画像"
+                className={styles.logo}
               />
-            </div>
-          ))
-        : images_sp.map((image, index) => (
-            <div
-              key={image}
-              className={`${
-                index === currentSlide
-                  ? styles.activeSlide
-                  : styles.inactiveSlide
-              } ${styles.mv_sp}`}
-            >
-              <Image
-                src={image}
-                alt={`Slide ${index + 1}`}
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          ))}
+            </FadeIn>
+            <MvTitle />
+          </div>
+          {isPc
+            ? images_pc.map((image, index) => (
+                <div
+                  key={image}
+                  className={`${
+                    index === currentSlide
+                      ? styles.activeSlide
+                      : styles.inactiveSlide
+                  } ${styles.mv_pc}`}
+                >
+                  <Image
+                    src={image}
+                    alt={`Slide ${index + 1}`}
+                    className={styles.slide_image}
+                    onLoadingComplete={handleImageLoad}
+                  />
+                </div>
+              ))
+            : images_sp.map((image, index) => (
+                <div
+                  key={image}
+                  className={`${
+                    index === currentSlide
+                      ? styles.activeSlide
+                      : styles.inactiveSlide
+                  } ${styles.mv_sp}`}
+                >
+                  <Image
+                    src={image}
+                    alt={`Slide ${index + 1}`}
+                    className={styles.slide_image}
+                    onLoadingComplete={handleImageLoad}
+                  />
+                </div>
+              ))}
+        </>
+      )}
     </section>
   );
 }
