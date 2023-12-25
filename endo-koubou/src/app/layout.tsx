9012a176757761bfc94404e143726964b9cab621
PK-Yakkun <mokkun515@gmail.com>
2023-12-26 01:42:52 +0900

    update: メタ編集とOG画像仮設置

--- endo-koubou/src/app/layout.tsx
+++ endo-koubou/src/app/layout.tsx
@@ -12,19 +12,38 @@ const notoSansJp = Noto_Sans_JP({
   display: "swap",
 });
 
+const siteName = "ENDO工房";
+const description =
+  "関西を中心に舞台美術の制作活動を行なっている、ENDO工房の公式Webサイトです。";
+const url = "https://endo-koubou.com";
+
 export const metadata: Metadata = {
   title: {
-    default: "ENDO工房",
+    default: siteName,
     template: "ENDO工房｜%s",
   },
-  description:
-    "関西を中心に舞台美術の制作活動を行なっている、ENDO工房の公式Webサイトです。",
+  description: description,
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
   robots: {
     index: false,
     googleBot: {
       index: false,
     },
+  },
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteName,
+    description,
+    site: "@elton_0914",
+    creator: "@elton_0914",
   },
 };
 
