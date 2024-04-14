a22ffe775f6c114f1ec4b6d427d1575ff3b0a147
PK-Yakkun <mokkun515@gmail.com>
2024-04-14 17:48:29 +0900

    update: MicroCMSのAPIスキーマ改修に伴い型の修正とlimit, offset. fillterなど引数の追加

--- endo-koubou/src/app/interface/work.ts
+++ endo-koubou/src/app/interface/work.ts
@@ -4,22 +4,37 @@ type ImageData = {
   height: number;
 };
 
+export type ProductionType = "舞台制作" | "個人制作";
+export interface WorksResponse {
+  contents: Work[];
+  totalCount: number;
+  offset: number;
+  limit: number;
+}
+
 export interface Work {
   id: string;
+  productionGenre: ProductionType;
   title: string;
   imageData: ImageData;
-  concept: string;
-  date: string;
-  companyName: string;
-  theaterName: string;
-  performancePeriod: string;
-  creater: string;
-  director: string;
-  lightingDesigner: string;
+  concept?: string;
+  date?: string;
+  companyName?: string;
+  theaterName?: string;
+  performancePeriod?: string;
+  creater?: string;
+  director?: string;
+  lightingDesigner?: string;
+  motif?: string;
+  materialsUsed?: string;
+  width?: string;
+  height?: string;
+  depth?: string;
+  productionPeriod?: string;
+  archiveImages?: ImageData[];
+  productionImages?: ImageData[];
   createdAt: string;
   updatedAt: string;
   publishedAt: string;
   revisedAt: string;
-  archiveImages?: ImageData[];
-  productionImages?: ImageData[];
 }
--- endo-koubou/src/app/util/api.ts
+++ endo-koubou/src/app/util/api.ts
@@ -1,11 +1,33 @@
-export async function apiCall(path: string, limit?: number) {
-  let queryParams = "";
+export async function apiCall(
+  path: string,
+  limit?: number,
+  offset?: number,
+  filters?: { fieldName: string; operator: string; value: string }
+) {
+  let queryParams = new URLSearchParams();
+
+  // limitが指定されている場合、クエリパラメータに追加
   if (limit) {
-    queryParams = `?limit=${limit}`;
+    queryParams.set("limit", limit.toString());
   }
 
+  // offsetが指定されている場合、クエリパラメータに追加
+  if (offset !== undefined) {
+    // offsetが0の場合も考慮するために、undefinedでチェック
+    queryParams.set("offset", offset.toString());
+  }
+
+  // filtersオブジェクトが指定されている場合、クエリパラメータに追加
+  if (filters) {
+    const filterValue = `${filters.fieldName}[${filters.operator}]${filters.value}`;
+    queryParams.set("filters", filterValue);
+  }
+
+  const queryString = queryParams.toString();
+  const queryPrefix = queryString ? `?${queryString}` : "";
+
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_MICROCMS_END_POINT}/${path}${queryParams}`,
+    `${process.env.NEXT_PUBLIC_MICROCMS_END_POINT}/${path}${queryPrefix}`,
     {
       headers: {
         "Content-Type": "application/json",
